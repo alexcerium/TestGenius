500d15a083554f8848b7e96bc0d047db5584c9e9
alexcerium <alexmatkava@gmail.com>
2024-05-24 19:55:02 +0300

    Initial Commit

---
+++ TestGenius/TestGeniusApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGeniusApp.swift
+//  TestGenius
+//
+//  Created by Aleksandr Matkava on 24.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestGeniusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
